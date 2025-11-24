8ceca094627716ea0c8e475f4db427c6b477e74c
Arqam7159 <145322998+Arqam7159@users.noreply.github.com>
2025-11-25 00:20:50 +0500

    Add files via upload

---
+++ Atm.h
@@ -0,0 +1,54 @@
+#ifndef ATM_H
+#define ATM_H
+
+#include <string>
+#include "Bank.h"
+#include "Card.h"
+
+class ATM {
+private:
+    Bank& bank;
+    double cashAvailable;
+    int transactionCounter;
+
+public:
+    ATM(Bank& bankRef) : bank(bankRef), cashAvailable(10000.0), transactionCounter(0) {}
+
+    void refillCash(double amount) {
+        if (amount > 0) {
+            cashAvailable += amount;
+        }
+    }
+
+    bool canDispense(double amount) const {
+        return amount <= cashAvailable && amount > 0;
+    }
+
+    double getCashAvailable() const {
+        return cashAvailable;
+    }
+
+    bool dispenseCash(double amount) {
+        if (canDispense(amount)) {
+            cashAvailable -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    void acceptCash(double amount) {
+        if (amount > 0) {
+            cashAvailable += amount;
+        }
+    }
+
+    Bank& getBank() {
+        return bank;
+    }
+
+    std::string generateTransactionID() {
+        return "TXN" + std::to_string(++transactionCounter);
+    }
+};
+
+#endif // ATM_H
